4f7ace4dbb890153f13c28088274637ccbaded9f
Garragames <96148865+garragames@users.noreply.github.com>
2023-06-12 14:04:59 -0600

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -22,8 +22,8 @@ namespace autoBot {
     const PARKING_GEAR = 0;
     const REVERSE_GEAR = 20;
 
-    const ECHO_PIN     = 1;
-    const TRIGGER_PIN  = 0;
+    const ECHO_PIN     = 0;
+    const TRIGGER_PIN  = 1;
     const MAX_DISTANCE = 500
 
     /**
