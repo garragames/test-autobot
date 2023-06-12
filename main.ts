e678974aa2b2b7d8d5beaa81cd99204d30b40a34
Garragames <96148865+garragames@users.noreply.github.com>
2023-06-12 17:07:25 -0600

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -376,23 +376,27 @@ namespace autoBot {
      * Send a ping and get the echo time (in microseconds) as a result
      * @param trig tigger pin
      * @param echo echo pin
+     * @param unit desired conversion unit
      * @param maxCmDistance maximum distance in centimeters (default is 500)
-     * 
      */
-    //% blockId=senseUltrasonic
-    //% block="ultrasonic sensor"
-    //% group="Sensors"
-    export function senseUltrasonic(): number {
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
-        pins.setPull(1, PinPullMode.PullNone);
-        pins.digitalWritePin(0, 0);
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
         control.waitMicros(2);
-        pins.digitalWritePin(0, 1);
+        pins.digitalWritePin(trig, 1);
         control.waitMicros(5);
-        pins.digitalWritePin(0, 0);
+        pins.digitalWritePin(trig, 0);
+
         // read pulse
-        const d = pins.pulseIn(1, PulseValue.High, 500 * 58);
-        return Math.idiv(d, 58);
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            case PingUnit.Inches: return Math.idiv(d, 148);
+            default: return d;
+        }
     }
 
     /**
